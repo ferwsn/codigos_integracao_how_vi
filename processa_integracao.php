31d3ac886e24eff2cf4cf97c366012a5795d38f0
Fernando Walter <fernandowalter04@outlook.com>
2025-06-29 18:07:28 -0300

    Update processa_integracao.php

--- processa_integracao.php
+++ processa_integracao.php
@@ -1,18 +1,18 @@
 <?php
 $nome = $_POST['nome'];
-$cpf = $_CPF['CPF'];
-$email = $_email['email'];
+$cpf = $_POST['CPF'];
+$email = $_POST['email'];
 
 try {
-    $conn = new PDO("sqlsrv:Server=POSITIVO\SQLEXPRESS;Database=db_asterix","integracao_php","php123");
+    $conn = new PDO("sqlsrv:Server=POSITIVO\\SQLEXPRESS;Database=db_asterix", "integracao_php", "php123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = "INSERT INTO cliente (nome, cpf, email) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$nome, $cpf, $email]);
-    
+
     echo "Dados inseridos com sucesso!";
 } catch (PDOException $e) {
-    echo "Erro: " . $e->getmessage()
+    echo "Erro: " . $e->getMessage();
 }
 ?>
